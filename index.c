a8291b691e872f957273459c6749e111a824c0b2
Ashu Sharma <105367150+mrmrashu@users.noreply.github.com>
2023-02-25 14:52:05 +0530

    Added Outline and some color & text changes

--- index.c
+++ index.c
@@ -41,7 +41,7 @@ static void
 
 		// Create a new window and set it's title
 		window = gtk_application_window_new(app);
-		gtk_window_set_title (GTK_WINDOW (window), "Ashu's App");
+		gtk_window_set_title (GTK_WINDOW (window), "Dear-Diary");
 		gtk_window_set_default_size(GTK_WINDOW (window), 500, 200);
 		gtk_container_set_border_width (GTK_CONTAINER (window), 10);
 
@@ -51,7 +51,7 @@ static void
 		// Pack the container in a window
 		gtk_container_add (GTK_CONTAINER (window), grid);
 		
-		button = gtk_button_new_with_label ("Print it");
+		button = gtk_button_new_with_label ("New Entry");
 		g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
 
 		/*
@@ -60,7 +60,7 @@ static void
 		*/
 		gtk_grid_attach (GTK_GRID (grid), button, 0, 0, 1, 1);
 
-		button = gtk_button_new_with_label ("Quit");
+		button = gtk_button_new_with_label ("Open Entry");
 		g_signal_connect_swapped (button, "clicked", G_CALLBACK(gtk_widget_destroy), window);
 		gtk_grid_attach (GTK_GRID (grid), button, 0, 1, 2, 1);
 		gtk_widget_show_all(window);
@@ -73,7 +73,7 @@ int main(int argc, char ** argv)
 	GtkApplication *app;
 	int ret;
 	// Giving the application a unique ID to assosiate with
-	app = gtk_application_new("org.ashu", G_APPLICATION_DEFAULT_FLAGS);
+	app = gtk_application_new("org.ashu", G_APPLICATION_FLAGS_NONE);
 	g_signal_connect (app, "activate", G_CALLBACK(activate), NULL);
 	ret = g_application_run(G_APPLICATION(app), argc, argv);
 
