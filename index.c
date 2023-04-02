b44f5e711be92755aea5661c11e6b0505b664c7f
Naman Chhabra <namanchhabra260801@gmail.com>
2023-04-02 12:46:07 +0530

    replace DEFAULT_FLAG with FLAG_NONE in index.c

--- index.c
+++ index.c
@@ -73,7 +73,7 @@ int main(int argc, char ** argv)
 	GtkApplication *app;
 	int ret;
 	// Giving the application a unique ID to assosiate with
-	app = gtk_application_new("org.ashu", G_APPLICATION_DEFAULT_FLAGS);
+	app = gtk_application_new("org.ashu", G_APPLICATION_FLAGS_NONE);
 	g_signal_connect (app, "activate", G_CALLBACK(activate), NULL);
 	ret = g_application_run(G_APPLICATION(app), argc, argv);
 
